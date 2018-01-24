5a5ba66a315c80c588ec1703f38dd06524675cc8
LimSeonghe <kettetoto@daum.net>
2018-01-24 17:14:19 +0900

    VO update 1014임성

--- src/main/java/kr/co/gdfm/movie/model/Movie.java
+++ src/main/java/kr/co/gdfm/movie/model/Movie.java
@@ -5,128 +5,82 @@ import java.util.Date;
 //영화 
 public class Movie {
 
- // 영화id 
- private Integer movieId;
+	private int Movie_id;
+	private String Movie_name;
+	private int Movie_genre_code;
+	private String Movie_grade;
+	private String Movie__story;
+	private String Movie_trailer;
+	private String Movie_show_yn;
+	private String Movie_release_date;
+	private int Movie_price;
+	private String Movie_demension;
+	private String Movie_ko_name;
+	public int getMovie_id() {
+		return Movie_id;
+	}
+	public void setMovie_id(int movie_id) {
+		Movie_id = movie_id;
+	}
+	public String getMovie_name() {
+		return Movie_name;
+	}
+	public void setMovie_name(String movie_name) {
+		Movie_name = movie_name;
+	}
+	public int getMovie_genre_code() {
+		return Movie_genre_code;
+	}
+	public void setMovie_genre_code(int movie_genre_code) {
+		Movie_genre_code = movie_genre_code;
+	}
+	public String getMovie_grade() {
+		return Movie_grade;
+	}
+	public void setMovie_grade(String movie_grade) {
+		Movie_grade = movie_grade;
+	}
+	public String getMovie__story() {
+		return Movie__story;
+	}
+	public void setMovie__story(String movie__story) {
+		Movie__story = movie__story;
+	}
+	public String getMovie_trailer() {
+		return Movie_trailer;
+	}
+	public void setMovie_trailer(String movie_trailer) {
+		Movie_trailer = movie_trailer;
+	}
+	public String getMovie_show_yn() {
+		return Movie_show_yn;
+	}
+	public void setMovie_show_yn(String movie_show_yn) {
+		Movie_show_yn = movie_show_yn;
+	}
+	public String getMovie_release_date() {
+		return Movie_release_date;
+	}
+	public void setMovie_release_date(String movie_release_date) {
+		Movie_release_date = movie_release_date;
+	}
+	public int getMovie_price() {
+		return Movie_price;
+	}
+	public void setMovie_price(int movie_price) {
+		Movie_price = movie_price;
+	}
+	public String getMovie_demension() {
+		return Movie_demension;
+	}
+	public void setMovie_demension(String movie_demension) {
+		Movie_demension = movie_demension;
+	}
+	public String getMovie_ko_name() {
+		return Movie_ko_name;
+	}
+	public void setMovie_ko_name(String movie_ko_name) {
+		Movie_ko_name = movie_ko_name;
+	}
 
- // 영화이름 
- private String movieName;
-
- // 영화장르code 
- private Integer movieGenreCode;  
-
- // 상영등급 
- private String movieGrade;
-
- // 영화줄거리 
- private String movieStory;
-
- // 영화트레일러 
- private String movieTrailer;
-
- // 영화상영여부 
- private String movieShowYn;
-
- // 영화개봉일 
- private Date movieReleaseDate;
-
- // 영화관람가격 
- private Integer moviePrice;
-
- // 영화차원종류 
- private String movieDemension;
-
- public Integer getMovieId() {
-     return movieId;
- }
-
- public void setMovieId(Integer movieId) {
-     this.movieId = movieId;
- }
-
- public String getMovieName() {
-     return movieName;
- }
-
- public void setMovieName(String movieName) {
-     this.movieName = movieName;
- }
-
- public Integer getMovieGenreCode() {
-     return movieGenreCode;
- }
-
- public void setMovieGenreCode(Integer movieGenreCode) {
-     this.movieGenreCode = movieGenreCode;
- }
-
- public String getMovieGrade() {
-     return movieGrade;
- }
-
- public void setMovieGrade(String movieGrade) {
-     this.movieGrade = movieGrade;
- }
-
- public String getMovieStory() {
-     return movieStory;
- }
-
- public void setMovieStory(String movieStory) {
-     this.movieStory = movieStory;
- }
-
- public String getMovieTrailer() {
-     return movieTrailer;
- }
-
- public void setMovieTrailer(String movieTrailer) {
-     this.movieTrailer = movieTrailer;
- }
-
- public String getMovieShowYn() {
-     return movieShowYn;
- }
-
- public void setMovieShowYn(String movieShowYn) {
-     this.movieShowYn = movieShowYn;
- }
-
- public Date  getMovieReleaseDate() {
-     return movieReleaseDate;
- }
-
- public void setMovieReleaseDate(Date movieReleaseDate) {
-     this.movieReleaseDate = movieReleaseDate;
- }
-
- public Integer getMoviePrice() {
-     return moviePrice;
- }
-
- public void setMoviePrice(Integer moviePrice) {
-     this.moviePrice = moviePrice;
- }
-
- public String getMovieDemension() {
-     return movieDemension;
- }
-
- public void setMovieDemension(String movieDemension) {
-     this.movieDemension = movieDemension;
- }
-
- // Movie 모델 복사
- public void CopyData(Movie param)
- {
-     this.movieId = param.getMovieId();
-     this.movieName = param.getMovieName();
-     this.movieGenreCode = param.getMovieGenreCode();
-     this.movieGrade = param.getMovieGrade();
-     this.movieStory = param.getMovieStory();
-     this.movieTrailer = param.getMovieTrailer();
-     this.movieShowYn = param.getMovieShowYn();
-     this.movieReleaseDate = param.getMovieReleaseDate();
-     this.moviePrice = param.getMoviePrice();
-     this.movieDemension = param.getMovieDemension();
- }
 }
